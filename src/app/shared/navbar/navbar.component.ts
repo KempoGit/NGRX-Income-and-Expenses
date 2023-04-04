d0eb2282782861883ed3bb899a19965617f94145
Kempo <gaston.nicolas.vega@gmail.com>
2023-04-03 22:06:27 -0300

    Fixed styles and photos

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription, filter } from 'rxjs';
+import { AppState } from 'src/app/app.reducer';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
+
+  userName: string | undefined;
+  userSubscription: Subscription;
+
+  constructor(private _Store: Store<AppState>) {
+    this.userSubscription = this._Store.select('auth')
+    .pipe(
+      filter( ({user}) => user !== null )
+    )
+    .subscribe(({user}) => {
+      this.userName = user?.name;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 
 }
